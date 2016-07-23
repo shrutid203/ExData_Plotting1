fae4dbd066da4b3e81d91121de8770162c406319
shrutid203 <shrutid203@gmail.com>
2016-07-24 02:09:40 +0530

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -32,3 +32,8 @@ lines(strptime(pwrcons$Timestamp, "%d/%m/%Y %H:%M:%S"), pwrcons$Sub_metering_3, 
 
 #Adds legend to graph
 legend("topright", lty= 1, col = c("Black", "red", "blue"), legend = c( "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
+ 
+
+dev.copy(png, file="plot3.png", width=480, height=480)
+dev.off()
